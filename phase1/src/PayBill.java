c642a39705888a69d23d1dfc1df068f29c566ead
sanxuwe7 <lisiyin3>
2019-03-05 17:33:57 -0500

    change access of begin method to default; add toString and getter of attribute to

--- phase1/src/PayBill.java
+++ phase1/src/PayBill.java
@@ -8,13 +8,26 @@ public class PayBill extends Transaction{
     }
 
     @Override
-    public void begin() {
+    void begin() {
         Loader.getAccount(this.getFromAccNum()).transferOut(this.getAmount());
     }
 
     @Override
     public Transaction reverse(){
         return null;
+        //throws exception?
     }
 
+    public String getTo() {
+        return to;
+    }
+
+    @Override
+    public String toString() {
+        String className = Loader.getAccount(this.getFromAccNum()).getClass().getName();
+        return ("time" + "pay " + this.getAmount() +
+                " to " + to + " from " + className + " "
+                + this.getFromAccNum() + "\n")
+                ;
+    }
 }
