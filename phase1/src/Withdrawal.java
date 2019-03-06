d091302d53f15ca4c890f8403381e6fe9bbf02fc
sanxuwe7 <lisiyin3>
2019-03-06 02:51:59 -0500

    Override abstract method selfCheck

--- phase1/src/Withdrawal.java
+++ phase1/src/Withdrawal.java
@@ -18,12 +18,43 @@ public class Withdrawal extends Transaction {
 
     @Override
     void begin() {
-        Loader.getAccount(this.fromAccNum).transferOut(this.getAmount());
+        Loader.getAccount(getFromAccNum()).transferOut(this.getAmount());
     }
 
     @Override
     public Deposit reverse() {
-        int toAcc = this.fromAccNum;
+        int toAcc = getFromAccNum();
         return new Deposit(toAcc, this.getAmount());
+    }
+
+    @Override
+    public boolean selfCheck() {
+        boolean possible  = false;
+        Account acc = Loader.getAccount(getFromAccNum());
+        int balance = acc.getBalance();
+        if (acc instanceof TransferOutable) {
+            if (acc instanceof DebtAccount) {
+                possible = debtCheck((DebtAccount)acc, getAmount(), balance);
+            } else if (acc instanceof ChequingAccount) {
+                possible = chequingCheck((ChequingAccount)acc, getAmount(), balance);
+            } else if (acc instanceof SacingAccount) {
+                possible = savingCheck(getAmount(), balance);
+            }
+        }
+        return possible;
+    }
+
+    private boolean debtCheck(DebtAccount acc, double amount, int balance){
+        int limit = acc.getLimit();
+        return (balance + amount - limit) <= 0;
+    }
+
+    private boolean chequingCheck(ChequingAccount acc, double amount, int balance){
+        int limit = acc.overDraftLimit();
+        return ((balance - amount >= limit) && (balance >= 0));
+    }
+
+    private boolean savingCheck(double amount, int balance){
+        return (balance - amount) >= 0;
     }
 }
