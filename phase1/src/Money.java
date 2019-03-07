939b0b6fd9a2b60399dbfc210f739945e53769a4
jiangd21 <JDl19981006?>
2019-03-07 00:40:26 -0500

    build Money

---
+++ phase1/src/Money.java
@@ -0,0 +1,29 @@
+public class Money {
+    private int numFive;
+    private int numTen;
+    private int numTwenty;
+    private int numFifty;
+
+    public Money(int numFive, int numTen, int numTwenty, int numFifty) {
+        this.numFive = numFive;
+        this.numTen = numTen;
+        this.numTwenty = numTwenty;
+        this.numFifty = numFifty;
+    }
+
+    public int getNumFive() {
+        return numFive;
+    }
+
+    public int getNumTen() {
+        return numTen;
+    }
+
+    public int getNumTwenty() {
+        return numTwenty;
+    }
+
+    public int getNumFifty() {
+        return numFifty;
+    }
+}
