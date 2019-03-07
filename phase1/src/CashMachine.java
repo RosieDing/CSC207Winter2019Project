85077373452e2da416e3930a4bf5e211fad14a38
sanxuwe7 <lisiyin3>
2019-03-07 01:58:28 -0500

    Add withdrawCash and checkAmount

--- phase1/src/CashMachine.java
+++ phase1/src/CashMachine.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class CashMachine {
     private int numFiveD;
     private int numTenD;
     private int numTwentyD;
     private int numFiftyD;
+    private WriteTXT alertFile;
 
     public CashMachine(){}
 
@@ -29,11 +33,52 @@ public class CashMachine {
         return numFiftyD;
     }
 
-    public void getAmount(){
-        System.out.println("Amount of $5: " + getNumFiveD());
-        System.out.println("Amount of $10: " + getNumTenD());
-        System.out.println("Amount of $20: " + getNumTwentyD());
-        System.out.println("Amount of $50: " + getNumFiftyD());
+    public Money getAmount(){
+        return new Money(getNumFiveD(), getNumTenD(), getNumTwentyD(),getNumFiftyD());
     }
 
+    public void withdrawCash(int amount) {
+        // throws exception if amount is not multiple of 5;
+        int[] Dbox = possibleD(amount);
+        int[] DAmount = {getNumFiveD(), getNumTenD(), getNumTwentyD(), getNumFiftyD()};
+        int[] Dnum = new int[Dbox.length];
+        for (int i = (Dbox.length-1); i > 0; i--) {
+            Dnum[i] = (amount/Dbox[i]);
+            if ((DAmount[i] - Dnum[i]) >= 0) {
+                amount -= (Dbox[i]*Dnum[i]);
+            }
+        }
+        if (amount != 0) {
+            //throws exception
+        } else {
+            int[] numCopy = Arrays.copyOf(Dnum, 4);
+            Money m = new Money(DAmount[0] - numCopy[0], DAmount[1] - numCopy[1],
+                    DAmount[2] - numCopy[2], DAmount[3] - numCopy[3]);
+            setAmount(m);
+
+        }
+    }
+
+    private int[] possibleD(int amount) {
+        int[] doms = {5, 10, 20, 50};
+        if (amount<50 && amount>=20) {
+            int [] domie = Arrays.copyOf(doms, 3);
+            doms = domie;
+        } else if (amount < 20 && amount >= 10) {
+            int [] domie = Arrays.copyOf(doms, 2);
+            doms = domie;
+        } else if (amount == 5) {
+            int [] domie = Arrays.copyOf(doms, 1);
+            doms = domie;
+        }
+        return doms;
+    }
+
+    public void checkAmount() {
+        Money m = getAmount();
+        if (m.getNumFive() < 20) { warning("Five"); }
+        if (m.getNumTen() < 20) { warning("Ten"); }
+        if (m.getNumTwenty() < 20) { warning("Twenty"); }
+        if (m.getNumFifty() < 20) { warning("Fifty"); }
+    }
 }
