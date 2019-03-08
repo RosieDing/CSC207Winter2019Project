d632bee545076d172a5327a7a90b3150a3efb287
dingruos <Drose@2015>
2019-03-08 15:03:29 -0500

    refine chequing

--- phase1/src/ATM/ChequingAccount.java
+++ phase1/src/ATM/ChequingAccount.java
@@ -1,12 +1,91 @@
 package ATM;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.Loader;
+
 public class ChequingAccount extends AssetAccount{
-    private int accountNum;
     private double balance;
-    private double availableCredit;
+    private double overDraftLimit = 100;
+    private double availableCredit = overDraftLimit + balance;
+    private int ownerID;
+    private boolean isPrimary = false;
+    private User owner= Loader.get(ownerID);
+    private String id = "003" + ownerID + String.valueOf(owner.getAccountNum + 1);
+    private final int AccountNum = Integer.valueOf(id);
 
     ChequingAccount(int ownerID){
         super(ownerID);
+        this.ownerID = ownerID;
     }
+
+
+    public void setoverDraftLimit(double limit){
+        this.overDraftLimit = limit;
+        setChanged();
+        notifyObservers();
+    }
+
+    public double getOverDraftLimit() {
+        return overDraftLimit;
+    }
+
+    public void setPrimary(boolean primary) {
+        isPrimary = primary;
+        setChanged();
+        notifyObservers();
+    }
+
+    public boolean getPrimary(){
+        return isPrimary;
+    }
+
+    @Override
+    public void setBalance(double balance) {
+        this.balance = balance;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public double getAvailableCredit(){
+        return availableCredit;
+    }
+    @Override
+    public int getAccountNum(){
+        return AccountNum;
+    }
+
+    @Override
+    public void transferOut(double amount){
+        balance += amount;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public double getBalance(){
+        return balance;
+    }
+
+    @Override
+    public void transferIn(double amount){
+        balance -= amount;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public void pay(double amount){
+        balance -= amount;
+        setChanged();
+        notifyObservers();
+    }
+
+    @Override
+    public void withdraw(double amount){
+        balance -= amount;
+        setChanged();
+        notifyObservers();
+    }
+
 
 }
