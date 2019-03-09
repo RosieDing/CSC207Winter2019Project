4d05d38f4d3dd46fdec375ef1e4c7fd3c1904925
Charles <tristanc2012@hotmail.com>
2019-03-09 17:57:27 -0500

    mistake

--- phase1/src/ATM/Accounts/LineOfCredit.java
+++ phase1/src/ATM/Accounts/LineOfCredit.java
@@ -10,13 +10,12 @@ public class LineOfCredit extends DebtAccount implements TransferOutable {
     private User owner= Loader.get(ownerID);
     private String id = "002" + ownerID + String.valueOf(owner.getAccountNum() + 1);
     private final int accountNum = Integer.valueOf(id);
+    private double availableCredit = limit - balance;
+    private double balance;
 
-
-    public LineOfCredit(int ownerID, int limit){
-        super(ownerID, limit);
-        this.ownerID = ownerID;
+    public LineOfCredit(int ownerID, int limit) {
+        super(ownerID, limit, availableCredit);
     }
-
     @Override
     public void transferOut(double amount){
         double newbalance = getBalance() + amount;
