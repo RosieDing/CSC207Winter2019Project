eb8ced53960e5ec97ef68004a989a54505500dd3
tuteng <albert.tu@mail.utoronto.ca>
2019-03-09 16:45:29 -0500

    loading module updated, adder added

--- phase1/src/ATM/InfoStorer.java
+++ phase1/src/ATM/InfoStorer.java
@@ -1,4 +1,37 @@
 package ATM;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Managing the saving and loading of all information
+ * Including accounts, users
+ */
 public class InfoStorer {
+    /**A mapping of Account ID to Account */
+    private Map<String, Account> accountMap;
+
+    /**A mapping of User ID to User */
+    private Map<String, User> userMap;
+
+    public InfoStorer(){
+        this.accountMap = new HashMap<String, Account>();
+        this.userMap = new HashMap<String, User>();
+    }
+
+    public Map<String, Account> getAccountMap() {
+        return accountMap;
+    }
+
+    public Map<String, User> getUserMap() {
+        return userMap;
+    }
+
+    public void addUser(User newUser){
+        this.userMap.put(String.valueOf(newUser.getId()), newUser);
+    }
+
+    public void addAccount(Account newAccount){
+        this.accountMap.put(String.valueOf(newAccount.getAccountNum()), newAccount);
+    }
 }
--- phase1/src/ATM/Loader.java
+++ phase1/src/ATM/Loader.java
@@ -1,4 +1,77 @@
 package ATM;
 
+import java.io.*;
+
 public class Loader {
+    private InfoStorer info;
+
+    public Loader(String filePath) throws ClassNotFoundException{
+        info = new InfoStorer();
+        File file = new File(filePath);
+        if (file.exists()) {
+            readFromFile(filePath);
+        } else {
+            try {
+                file.createNewFile();
+            } catch (IOException ex) {
+                System.out.println(ex);;
+            }
+        }
+    }
+
+    public void readFromFile(String path) throws ClassNotFoundException {
+        try {
+            InputStream file = new FileInputStream(path);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            //deserialize the InfoStorer
+            info = (InfoStorer) input.readObject();
+            input.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void saveToFile(String filePath) throws IOException {
+
+        OutputStream file = new FileOutputStream(filePath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutput output = new ObjectOutputStream(buffer);
+
+        // serialize the Map
+        output.writeObject(info);
+        output.close();
+    }
+
+    public InfoStorer getInfo() {
+        return info;
+    }
+
+    public User getUser(int id){
+        String key = String.valueOf(id);
+        return info.getUserMap().get(key);
+    }
+
+    public Account getAccount(int id){
+        String key = String.valueOf(id);
+        return info.getAccountMap().get(key);
+    }
+
+    public int getUserNum(){
+        return info.getUserMap().size();
+    }
+
+    public int getAccountNum(){
+        return info.getAccountMap().size();
+    }
+
+    public void add(User newUser){
+        info.addUser(newUser);
+    }
+
+    public void add(Account newAccount){
+        info.addAccount(newAccount);
+    }
 }
+
