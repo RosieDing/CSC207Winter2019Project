201bad8a1f684508bebaad2b33b9f3ff4d925669
Charles <tristanc2012@hotmail.com>
2019-03-10 05:50:27 -0400

    small changes

--- phase1/src/ATM/Accounts/SavingAccount.java
+++ phase1/src/ATM/Accounts/SavingAccount.java
@@ -2,31 +2,35 @@ package ATM.Accounts;
 
 import ATM.InfoHandling.InfoManager;
 
-
-
+/**
+ * Saving account class
+ */
 public class SavingAccount extends AssetAccount {
     private double availableCredit = getBalance();
     private ISaverPlan iSaverPlan;
     private final String accountNum = "004" + getOwnerID() + (InfoManager.getInfoManager().getAccountNum() + 1);
 
+    /**Constructor for saving account */
     public SavingAccount(String ownerID, ISaverPlan s){
         super(ownerID);
         this.iSaverPlan = s;
         setBalance(iSaverPlan.compute(getBalance()));
     }
 
-
+    /**Getter method for available credit */
     @Override
     public double getAvailableCredit() {
         return availableCredit;
     }
 
+    /**Getter method for account number */
     @Override
     public String getAccountNum(){
         return accountNum;
     }
 
-    public void setiSaverPlan(ISaverPlan s) {
+    /**Setter method for saver plan */
+    public void setSaverPlan(ISaverPlan s) {
         this.iSaverPlan = s;
     }
 
