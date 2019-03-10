5bb3617873ab07c1607d25bd64d908aa88da95a6
dingruos <Drose@2015>
2019-03-09 23:21:08 -0500

    DespositReader edit

--- phase1/src/ATM/InfoHandling/DepositReader.java
+++ phase1/src/ATM/InfoHandling/DepositReader.java
@@ -18,7 +18,6 @@ import ATM.InfoHandling.*;
 /** */
 public class DepositReader {
     private Map<Depositable, Double> disposedAccounts = new HashMap();
-
         public void txtReader(String fileName) throws IOException {
             Path path = Paths.get(fileName);
             try (BufferedReader fileInput = Files.newBufferedReader(path)) {
@@ -26,13 +25,11 @@ public class DepositReader {
                 int length = line.length();
                 while (line != null) {
                     String[] c = line.split(" ");
-                    for (String a : c) {
                         for (int b = 0; b <= length; b = +2) {
-                            Depositable d = InfoManager.getAccount(Integer.valueOf(c[b]));
+                            Depositable d = InfoManager.getAccount(Integer.valueOf(b));
                             Double k = Double.valueOf(c[b + 1]);
                             disposedAccounts.put(d, k);
                         }
-                    }
                     line = fileInput.readLine();
                 }
             } catch (IOException ex) {
