dafdb3904949b8ab730428e6db6685b241515164
Charles <tristanc2012@hotmail.com>
2019-03-10 08:35:15 -0400

    documentation

--- phase1/src/ATM/Accounts/Account.java
+++ phase1/src/ATM/Accounts/Account.java
@@ -15,7 +15,12 @@ public abstract class Account implements Withdrawable, TransferInable, Payable, 
     /**The ID of the User whom this account belongs to */
     private String ownerID;
 
-    /**Constructor of account */
+    /**
+     * Constructor of account
+     * Create a new account with ownerID
+     *
+     * @param ownerID the ID of the owner
+     */
     public Account(String ownerID){
         this.ownerID = ownerID;
     }
@@ -29,19 +34,27 @@ public abstract class Account implements Withdrawable, TransferInable, Payable, 
         return dateOfCreation;
     }
 
+    /**Abstract Method for deposit money to account*/
     public abstract void deposit(double amount);
 
+    /**Abstract Method for getting available credit from account */
     public abstract double getAvailableCredit();
 
+    /**Abstract Method for getting account number from account */
     public abstract String getAccountNum();
 
+    /**Abstract Method for getting balance from account */
     public abstract double getBalance();
 
+    /**Abstract Method for setting balance of account */
     public abstract void setBalance(double amount);
 
+    /**Abstract Method for transfering money to account */
     public abstract void transferIn(double amount);
 
+    /**Abstract Method for paying money from account */
     public abstract void pay(double amount);
 
+    /**Abstract Method for withdraw money from account */
     public abstract void withdraw(double amount);
 }
