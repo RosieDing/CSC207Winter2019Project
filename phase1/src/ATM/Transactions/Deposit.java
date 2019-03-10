f1299344e9a9b3564ed353598079c093eca7ce9f
sanxuwe7 <lisiyin3>
2019-03-10 04:36:32 -0400

    Fix bug

--- phase1/src/ATM/Transactions/Deposit.java
+++ phase1/src/ATM/Transactions/Deposit.java
@@ -1,15 +1,17 @@
 package ATM.Transactions;
 
 import ATM.Accounts.Account;
+import ATM.Accounts.ChequingAccount;
+import ATM.Accounts.Depositable;
 
 import java.time.LocalDateTime;
 
 public class Deposit extends Transaction{
     private final Account fromAcc;
-    private final Account toAcc;
+    private final Depositable toAcc;
     private final LocalDateTime time;
 
-    public Deposit(Account toAccount, double amount) {
+    public Deposit(Depositable toAccount, double amount) {
         super(amount);
         this.toAcc = toAccount;
         this.fromAcc = null;
@@ -34,23 +36,39 @@ public class Deposit extends Transaction{
     public Account getFromAcc() { return fromAcc; }
 
     public Account getToAcc() {
-        return toAcc;
+        return (Account)toAcc;
     }
 
     public LocalDateTime getTime() {
         return time;
     }
 
+    /***
+     * Execute this transaction. Set field happened as true if
+     * this transaction is made.
+     */
     @Override
     void begin() {
         getToAcc().deposit(this.getAmount());
         setHappened(true);
     }
 
+    /***
+     * Return a new transaction as a reverse of the input transaction (same amount,
+     * reversed from and to account). If the original "to account" has not enough
+     * money for undoing input transaction, throws ReverseNotPossibleException.
+     * @return Withdrawal
+     * @throws ReverseNotPossibleException
+     */
     @Override
     public Withdrawal reverse() throws ReverseNotPossibleException{
         Account fromAcc = getToAcc();
-        if (getAmount() > fromAcc.getBalance()) {
+        if (fromAcc instanceof ChequingAccount) {
+            if (fromAcc.getBalance() <=0) {
+                throw new ReverseNotPossibleException();
+            }
+        }
+        if (getAmount() > fromAcc.getAvailableCredit()) {
             throw new ReverseNotPossibleException();
         }
         return new Withdrawal(fromAcc, this.getAmount());
