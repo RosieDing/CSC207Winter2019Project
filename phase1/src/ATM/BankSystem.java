4a12114247ec39b916d7423d9e51113f72094946
tuteng <albert.tu@mail.utoronto.ca>
2019-03-10 08:06:53 -0400

    PromtUser created

--- phase1/src/ATM/BankSystem.java
+++ phase1/src/ATM/BankSystem.java
@@ -1,11 +1,26 @@
 package ATM;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BankSystem {
     private static boolean SystemOn;
 
     public static void main(String[] args) {
-        while (SystemOn){
+        String a = promptUser("name?");
+        System.out.println(a);
+    }
 
+    private static String promptUser(String prompt){
+        BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(prompt);
+        try{
+            String input = kbd.readLine();
+            kbd.close();
+            return input;
+        } catch(IOException ex) {
+            return ex.toString();
         }
     }
 }
