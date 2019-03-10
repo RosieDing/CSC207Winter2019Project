241b5b5d1d6294d3df14875ae41e08f31a7fa4bd
jiangd21 <JDl19981006?>
2019-03-10 01:03:07 -0500

    change

--- phase1/src/ATM/BankIdentities/BankIdentity.java
+++ phase1/src/ATM/BankIdentities/BankIdentity.java
@@ -2,7 +2,7 @@ package ATM.BankIdentities;
 
 import java.util.Observable;
 
-public abstract class BankIdentity extends Observable {
+public abstract class BankIdentity {
     public abstract String getId();
 
 }
