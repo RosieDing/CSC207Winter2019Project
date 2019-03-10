70ccbfe27e9c90015db5c1c714b29355dc2470e5
dingruos <Drose@2015>
2019-03-10 09:51:28 -0400

    Documentation

--- phase1/src/ATM/BankIdentities/AlreadyPrimaryException.java
+++ phase1/src/ATM/BankIdentities/AlreadyPrimaryException.java
@@ -1,8 +1,13 @@
 package ATM.BankIdentities;
-
+/** This is a AlreadyPrimaryException */
 public class AlreadyPrimaryException extends Exception {
     public AlreadyPrimaryException() {
     }
+
+    /** Initialize a AlreadyPrimaryException
+     *
+     * @param message the message send
+     */
 
     public AlreadyPrimaryException(String message) {
         super(message);
--- phase1/src/ATM/BankIdentities/BankIdentity.java
+++ phase1/src/ATM/BankIdentities/BankIdentity.java
@@ -3,6 +3,7 @@ package ATM.BankIdentities;
 import java.io.Serializable;
 import java.util.Observable;
 
+/** BankIdentity abstract class */
 public abstract class BankIdentity implements Serializable {
     public abstract String getId();
 
