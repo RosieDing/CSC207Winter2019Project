5404c08a9cb96b53ec9439b9d2f07064c02c38e3
jiangd21 <JDl19981006?>
2019-03-21 21:05:02 -0400

    factor the account creator out

--- phase2/src/ATM/BankIdentities/BankManager.java
+++ phase2/src/ATM/BankIdentities/BankManager.java
@@ -52,7 +52,7 @@ public class BankManager extends BankIdentity {
      * And BankManger could init the PassWord of the user with "1234".
      * */
     public void createUser() {
-        AccountCreater creat = new AccountCreater();
+        AccountCreator creat = new AccountCreator();
         User u = new User();
         UserAccManager accM = new UserAccManager(u.getId());
         u.setAccManager(accM);
@@ -114,80 +114,5 @@ public class BankManager extends BankIdentity {
             System.out.println(e.getMessage());
         }
     }
-
-
-    /** Create New Chequing Account for the User
-     *
-     * @param userID the userID of the user
-     * @throws AlreadyPrimaryException
-     *  *//*
-
-    public void createNewChequingAccount(String userID) {
-        User u = InfoManager.getInfoManager().getUser(userID);
-        UserAccManager m = u.getAccManager();
-        ChequingAccount acc = new ChequingAccount(userID);
-        if (m.getPrimaryChq() == null){
-            try {
-                m.setPrimaryChq(acc);
-            } catch(AlreadyPrimaryException e ){
-                System.out.println();
-            }
-        }
-        try {
-            m.addAccount(acc);
-        } catch (UserNotOwnAccountException e) {
-            System.out.println(e);
-        }
-    }
-
-    *//** Create New Saving Account for the User
-     *
-     * @param userID the userID of the user
-     *  *//*
-    public void createNewSavingAccount(String userID){
-        User u = InfoManager.getInfoManager().getUser(userID);
-        UserAccManager m = u.getAccManager();
-        MonthlyInterest interest = new MonthlyInterest(0.01);
-        SavingAccount acc = new SavingAccount(userID, interest);
-        try {
-            m.addAccount(acc);
-        } catch (UserNotOwnAccountException e) {
-            System.out.println(e);
-        }
-    }
-
-
-    *//** Create New Credit Account for the User
-     *
-     * @param userID the userID of the user
-     * @param limit the limit of the credit account
-     *  *//*
-    public void createNewCreditAccount(String userID, double limit){
-        User u = InfoManager.getInfoManager().getUser(userID);
-        UserAccManager m = u.getAccManager();
-        CreditAccount acc = new CreditAccount(userID, limit);
-        try {
-            m.addAccount(acc);
-        } catch (UserNotOwnAccountException e) {
-            System.out.println(e);
-        }
-    }
-
-    *//** Create New Line of Credit Account for the User
-     *
-     * @param userID the userID of the user
-     * @param limit the limit of the line of credit account
-     *  *//*
-    public void createNewLineOfCredit(String userID, double limit){
-        User u = InfoManager.getInfoManager().getUser(userID);
-        UserAccManager m =u.getAccManager();
-        LineOfCredit acc = new LineOfCredit(userID, limit);
-        try {
-            m.addAccount(acc);
-        } catch (UserNotOwnAccountException e) {
-            System.out.println(e);
-        }
-    }*/
-
 }
 
