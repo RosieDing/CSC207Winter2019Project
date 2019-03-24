2b323d1f6e73f0d4c7d843cc761b5b2298f5773c
tuteng <albert.tu@mail.utoronto.ca>
2019-03-24 09:53:11 -0400

    Injection independency in BankIdentities

--- phase2/src/ATM/BankIdentities/BankEmployee.java
+++ phase2/src/ATM/BankIdentities/BankEmployee.java
@@ -3,19 +3,15 @@ package ATM.BankIdentities;
 public abstract class BankEmployee  extends BankIdentity{
     private String id;
 
-    public BankEmployee(){
-
-    }
-
     public String getId(){return id;}
 
     /**
-     * The method for the BankManger to create new_user, user_accounts_Manager
-     * with new accounts.
-     * And BankManger could init the PassWord of the user with "1234".
-     * */
+     * The method for the BankEmployee to create new_user with default password "1234"
+     * It will create a default primary chequing account
+     * At the same time, all the information is going to be updated to global information
+     */
     public void createUser() {
-        AccountCreator create = new AccountCreator();
+        AccountCreator accCreator = new AccountCreator();
         User u = new User();
         UserAccManager accM = new UserAccManager(u.getId());
         u.setAccManager(accM);
@@ -26,7 +22,7 @@ public abstract class BankEmployee  extends BankIdentity{
                 + " initial Password: " + "1234");
         infoManager.add(u);
         passM.addObserver(infoManager);
-        create.createNewChequingAccount(u.getId());
+        accCreator.createNewChequingAccount(u.getId());
 
 
     }
