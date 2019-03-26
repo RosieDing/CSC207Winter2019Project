f684349152b4e005bc50be7df26f158d15bf9303
dingruos <Drose@2015>
2019-03-26 14:58:19 -0400

    refactor the monthly interest Saving account

--- phase2/src/ATM/Accounts/ISaverPlans/MonthlyPremiumInterest.java
+++ phase2/src/ATM/Accounts/ISaverPlans/MonthlyPremiumInterest.java
@@ -4,33 +4,33 @@ import ATM.BankSystem.Time;
 
 import java.io.Serializable;
 
-/**
- * The normal saving plans with monthly interest less than 3%
- */
+/**High interest plan with monthly interesting rate above 3%*/
+public class MonthlyPremiumInterest implements ISaverPlan, Serializable {
+    private final double interestRate = 0.03;
 
-public class MonthlyInterest implements ISaverPlan, Serializable {
-    private final double interestRate = 0.01;
+
     private Time time = Time.getTime();
 
     /**
-     * Constructor for the monthly interest class
+     * Constructor for the PremiumHighInterest class
+     * [deposit more than 10000.00]
      * Creates monthly interest with interest rate
      */
-    public MonthlyInterest(){
-    }
 
     /** Calculate and return the amount of money that is earned from interest */
     @Override
     public double compute(double amount) {
         if (time.getSystemCurrentTime().getDayOfMonth() == 1){
-                return amount * (interestRate+1);
-            }
-            return amount;
+            return amount * (interestRate+1);
+        }
+        return amount;
     }
 
     /** Return a String representation of this saving plan
      * @return String information about this saving plan */
     public String toString(){
         return "This is a SavingPlan with monthly interest: "+interestRate;}
-    }
+}
+
+
 
