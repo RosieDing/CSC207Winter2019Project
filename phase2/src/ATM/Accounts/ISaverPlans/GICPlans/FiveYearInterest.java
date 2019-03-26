39eadafb3d1a9f359616c6d5f9c5dbe449c4dc49
dingruos <Drose@2015>
2019-03-26 18:05:29 -0400

    FiveYear

--- phase2/src/ATM/Accounts/ISaverPlans/GICPlans/FiveYearInterest.java
+++ phase2/src/ATM/Accounts/ISaverPlans/GICPlans/FiveYearInterest.java
@@ -11,39 +11,4 @@ public class FiveYearInterest extends GICPlan implements Serializable {
         setInterestRate(interestRate);
         setPeriodOfMonth(periodOfMonth);
     }
-//    @Override
-//    public double compute(double amount){
-//        return amount*(interestRate +1);
-//    }
-//
-//    public double computeTotalInterest(double amount){
-//        return amount*interestRate;
-//    }
-//
-//    public double computeMonthlyInterest(double amount){
-//        return amount*(interestRate+1)/periodOfMonth;
-//    }
-//
-//    public int getPeriodMonth(){return periodOfMonth;}
-//
-//    public double getPerspectTotalInterest(double amount, LocalDate dateOfCreation,boolean isEnd){
-//        if(!isEnd){
-//            Period p = Period.between(dateOfCreation, current);
-//            int month = p.getMonths() + 1;
-//            return month * computeMonthlyInterest(amount);
-//        }
-//        else {return computeTotalInterest(amount);}
-//    }
-//
-//    public double getPerspectBalance(double amount,LocalDate dateOfCreation, boolean isEnd){
-//        return amount + getPerspectTotalInterest(amount, dateOfCreation, isEnd);
-//    }
-//
-//    public int getMonthLeft(LocalDate dateOfCreation, boolean isEnd){
-//        if(!isEnd){
-//            return periodOfMonth - getMonthpassed(dateOfCreation);
-//        }
-//        else{return 0;}
-//    }
-
 }
