9e0be0b55f208d9ccbfcc572ca20d0a96c1ab291
jiangd21 <JDl19981006?>
2019-03-30 15:14:13 -0400

    create a new txt writer

---
+++ phase2/src/ATM/InfoHandling/RecordWriter.java
@@ -0,0 +1,41 @@
+package ATM.InfoHandling;
+
+import java.io.*;
+
+/***
+ * AlertWriter class
+ */
+public class RecordWriter implements WriteTXT, Serializable {
+    /***
+     * path of file to be written
+     */
+    private String path = "./record.txt";
+
+    /***
+     * Write the file with content.
+     * If the file does not exist, create a new one.
+     * @param content
+     */
+    public void write(String content){
+        File file = new File(path);
+        try{
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            PrintWriter out = new PrintWriter(new FileWriter(file, true));
+
+            out.append(content + "\n");
+            out.close();
+        }catch(IOException e){
+            System.out.println("Alert is not recorded.");
+        }
+    }
+
+    /***
+     * Set the path of file to be written
+     * @param newPath (String) path of expected file
+     */
+    public void setPath(String newPath) {
+        this.path = newPath;
+    }
+}
