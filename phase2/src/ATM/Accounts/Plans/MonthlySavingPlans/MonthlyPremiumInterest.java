111a1cdc2ad56b75d61ee0eaecbdeea9cc484dc7
dingruos <Drose@2015>
2019-03-31 18:45:56 -0400

    refactor saving plans

--- phase2/src/ATM/Accounts/Plans/MonthlySavingPlans/MonthlyPremiumInterest.java
+++ phase2/src/ATM/Accounts/Plans/MonthlySavingPlans/MonthlyPremiumInterest.java
@@ -6,21 +6,16 @@ import ATM.BankSystem.Date;
 import java.io.Serializable;
 
 /**High interest plan with monthly interesting rate above 3%*/
-public class MonthlyPremiumInterest implements Plan, Serializable {
+public class MonthlyPremiumInterest extends SavingPlan {
     private final double interestRate = 0.03;
 
-    /** Calculate and return the amount of money that is earned from interest */
-    @Override
-    public double compute(double amount) {
-        Date date = Date.getDate();
-        if (date.getSystemCurrentTime().getDayOfMonth() == 1){
-            return amount * (interestRate+1);
-        }
-        return amount;
+    public MonthlyPremiumInterest(){
+        setInterestRate(interestRate);
     }
 
     /** Return a String representation of this saving plan
      * @return String information about this saving plan */
+    @Override
     public String toString(){
         return "This is a SavingPlan with monthly interest: " + interestRate;
     }
