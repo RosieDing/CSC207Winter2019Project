6067c07f0b25171f887387ea5a1f2187c6bac29c
lisiyin3 <siying.li@mail.utoronto.ca>
2019-03-30 23:40:40 -0400

    Fix duplicate code

--- phase2/src/ATM/InfoHandling/RecordWriter.java
+++ phase2/src/ATM/InfoHandling/RecordWriter.java
@@ -19,13 +19,7 @@ public class RecordWriter implements WriteTXT, Serializable {
     public void write(String content){
         File file = new File(path);
         try{
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            PrintWriter out = new PrintWriter(new FileWriter(file, true));
-
-            out.append(content + "\n");
-            out.close();
+            writeHelper(file, content);
         }catch(IOException e){
             System.out.println("Alert is not recorded.");
         }
