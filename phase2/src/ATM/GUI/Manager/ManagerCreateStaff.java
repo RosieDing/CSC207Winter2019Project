efa7464d91ee70161786bfbc00945eb437092905
Charles <tristanc2012@hotmail.com>
2019-03-31 21:45:19 -0400

    GUI Update

--- phase2/src/ATM/GUI/Manager/ManagerCreateStaff.java
+++ phase2/src/ATM/GUI/Manager/ManagerCreateStaff.java
@@ -3,14 +3,11 @@ package ATM.GUI.Manager;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import ATM.BankIdentities.BankManager;
 import  ATM.InfoHandling.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -22,6 +19,9 @@ public class ManagerCreateStaff extends JFrame {
 	 * Create the frame.
 	 */
 	public ManagerCreateStaff(String id, InfoManager infoManager) {
+		InfoStorer infoStorer = infoManager.getInfoStorer();
+		BankManager bankManager = infoManager.getBankManager(id);
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -57,10 +57,37 @@ public class ManagerCreateStaff extends JFrame {
 		JRadioButton rdbtnINR = new JRadioButton("INR");
 		rdbtnINR.setBounds(380, 34, 70, 23);
 		contentPane.add(rdbtnINR);
-		
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnCAD);
+		group.add(rdbtnUSD);
+		group.add(rdbtnCNY);
+		group.add(rdbtnEUR);
+		group.add(rdbtnGBP);
+		group.add(rdbtnINR);
+
 		JButton btnCreateStaff = new JButton("Create Staff");
 		btnCreateStaff.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				String type = rdbtnCAD.getText();
+				if (rdbtnCAD.isSelected()) {
+					type = rdbtnCAD.getText();
+				}else if (rdbtnUSD.isSelected()) {
+					type = rdbtnUSD.getText();
+				}else if (rdbtnCNY.isSelected()) {
+					type = rdbtnCNY.getText();
+				}else if (rdbtnEUR.isSelected()) {
+					type = rdbtnEUR.getText();
+				}else if (rdbtnGBP.isSelected()) {
+					type = rdbtnGBP.getText();
+				}else if (rdbtnINR.isSelected()) {
+					type = rdbtnINR.getText();
+				}
+
+				String ID = bankManager.createBankStaff(type, infoStorer);
+				JOptionPane.showMessageDialog(null, "New staff created! user ID: " + ID);
+
+
 			}
 		});
 		btnCreateStaff.setBounds(190, 68, 117, 29);
