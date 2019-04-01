a2422c6d331af4528a6a68f50ac458f20813b062
dingruos <Drose@2015>
2019-03-31 21:01:56 -0400

    withdrawal refine

--- phase2/src/ATM/GUI/User/UserTransactionWithdrawal.java
+++ phase2/src/ATM/GUI/User/UserTransactionWithdrawal.java
@@ -1,5 +1,8 @@
 package ATM.GUI.User;
 
+import ATM.AccountTypeChecker.PayableChecker;
+import ATM.AccountTypeChecker.TypeChecker;
+import ATM.AccountTypeChecker.WithdrawableChecker;
 import ATM.Accounts.Currency;
 import ATM.BankIdentities.UserAccManager;
 import ATM.InfoHandling.InfoManager;
@@ -7,11 +10,10 @@ import ATM.Machine.CashMachine;
 import ATM.Transactions.Transaction;
 import ATM.Transactions.TransactionManager;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.swing.*;
@@ -28,8 +30,8 @@ public class UserTransactionWithdrawal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public UserTransactionWithdrawal(Map<String, Object> transMap, String id,
-									 TransactionManager tm, CashMachine machine, InfoManager infoManager) {
+	public UserTransactionWithdrawal(Map<String, Object> transMap, UserAccManager uam, TransactionManager tm,
+									 CashMachine machine, String id, InfoManager infoManager) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -43,7 +45,7 @@ public class UserTransactionWithdrawal extends JFrame {
 				try {
 					Transaction trans = tm.makeTransaction(transMap, machine);
 					if (trans.isHappened()) {
-						JOptionPane.showMessageDialog(null, "Deposit successful!");
+						JOptionPane.showMessageDialog(null, "Withdraw successful!");
 					}
 				} catch (NullPointerException e) {
 					JOptionPane.showMessageDialog(rootPane, "Transaction is not possible.");
@@ -58,23 +60,19 @@ public class UserTransactionWithdrawal extends JFrame {
 		JLabel lblFromAccount = new JLabel("From Account:");
 		lblFromAccount.setBounds(6, 80, 110, 16);
 		contentPane.add(lblFromAccount);
-		//need to add accounts
 
 
-		txtID = new JTextField();
-		txtID.setColumns(10);
-		txtID.setBounds(104, 75, 130, 26);
-		contentPane.add(txtID);
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(110, 78, 110, 27);
+		contentPane.add(comboBox);
+		TypeChecker checker = new WithdrawableChecker();
+		ArrayList list = uam.getTypeAccounts(checker);
+		for (Object o : list) {
+			comboBox.addItem(o);
+		}
+		transMap.put("fromAccount", comboBox.getSelectedItem());
 		
-		txtAmount = new JTextField();
-		txtAmount.setColumns(10);
-		txtAmount.setBounds(90, 134, 130, 26);
-		contentPane.add(txtAmount);
-		
-		JLabel lblAmount = new JLabel("Amount:");
-		lblAmount.setBounds(28, 139, 60, 16);
-		contentPane.add(lblAmount);
-
 		txtAmount = new JTextField();
 		txtAmount.addKeyListener(new KeyAdapter() {
 			public void keyReleased(java.awt.event.KeyEvent evt) {
@@ -87,18 +85,14 @@ public class UserTransactionWithdrawal extends JFrame {
 			}
 
 		});
-
 		txtAmount.setColumns(10);
-		txtAmount.setBounds(125, 134, 130, 26);
+		txtAmount.setBounds(90, 134, 130, 26);
 		contentPane.add(txtAmount);
-		Currency amount = new Currency(Double.valueOf(txtAmount.getText()));
-		transMap.put("amount", amount);
-
 		
-//		txtID = new JTextField();
-//		txtID.setColumns(10);
-//		txtID.setBounds(104, 75, 130, 26);
-//		contentPane.add(txtID);
+		JLabel lblAmount = new JLabel("Amount:");
+		lblAmount.setBounds(28, 139, 60, 16);
+		contentPane.add(lblAmount);
+
 
 		JLabel lblWithdrawal = new JLabel("Withdrawal");
 		lblWithdrawal.setBounds(159, 22, 89, 16);
